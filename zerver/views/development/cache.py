7f2e4dde3389303170d9018329dc6042c154a6a1
Jainta Paul <research@openrefactory.com>
2023-01-12 14:03:12 +0000

    Fixed Security Misconfiguration Issues-1H

--- zerver/views/development/cache.py
+++ zerver/views/development/cache.py
@@ -11,7 +11,7 @@ from zerver.models import clear_client_cache, flush_per_request_caches
 ZULIP_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../")
 
 # This is used only by the Puppeteer tests to clear all the cache after each run.
-@csrf_exempt
+# Warning: CSRF protection should not be disabled on a view
 @require_post
 def remove_caches(request: HttpRequest) -> HttpResponse:
     cache = get_cache_backend(None)
